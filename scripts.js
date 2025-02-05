3921a71f1ff18d30b9bb325664647cdf3c4d9a37
811237 <811237@sylvaniastudents.org>
2025-02-05 13:51:43 -0500

    started function and set placeholder

---
+++ scripts.js
@@ -0,0 +1,16 @@
+// Global variable housing;
+
+    var bigList = [];
+
+// Function housing;
+
+    function thisFunc(){
+        event.preventDefault();
+        let list = document.getElementById('form').serializeArray();
+    }
+
+// Run on load housing;
+
+
+
+// Action-lead event housing;
