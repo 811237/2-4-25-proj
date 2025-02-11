1c013ecb3cd6526bb42482503342e92a1770c3fd
darkblood07 <8008622sylvaniastudents.org>
2025-02-11 12:59:16 -0500

    sdfghjk

--- scripts.js
+++ scripts.js
@@ -7,10 +7,22 @@
     function thisFunc(){
         event.preventDefault();
         let list = document.getElementById('form').serializeArray();
+        let formed = {
+            name: list[0].value,
+            email: list[1].value,
+            age: list[2].value,
+            message: list[3].value
+        };
+        bigList.push(formed);
+        localStorage.formies = JSON.stringify(bigList);
     }
 
 // Run on load housing;
 
+    if(localStorage.formies){
+        bigList = JSON.parse(localStorage.formies);
+    };
 
+    console.log(bigList);
 
 // Action-lead event housing;
